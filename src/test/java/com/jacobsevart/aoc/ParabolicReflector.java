27eb0d8ea3bf028f023a561858251572e963dc1a
Jacob Sevart <jacob.sevart@gmail.com>
2023-12-16 23:49:52 -0500

    Most of day 14 part II... just the wrong answer

--- src/test/java/com/jacobsevart/aoc/ParabolicReflector.java
+++ src/test/java/com/jacobsevart/aoc/ParabolicReflector.java
@@ -1,14 +1,90 @@
 package com.jacobsevart.aoc;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ParabolicReflector {
     List<List<Character>> grid;
+    Map<Character, Integer> counts;
+
     public ParabolicReflector(Scanner in) {
         grid = parse(in);
+        counts = countObjects();
+    }
+
+    public ParabolicReflector(List<List<Character>> grid) {
+        // copy the array in so it can be reused
+        this.grid = new ArrayList<>();
+        for (int i = 0; i < grid.size(); i++) {
+            List<Character> row = new ArrayList<>();
+            for (int j = 0; j < grid.get(0).size(); j++) {
+                row.add(grid.get(i).get(j));
+            }
+            this.grid.add(row);
+        }
+
+        counts = countObjects();
+    }
+
+    Map<Character, Integer> countObjects() {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.get(0).size(); j++) {
+                char c = grid.get(i).get(j);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                } else {
+                    map.put(c, 1);
+                }
+            }
+        }
+
+        return map;
+    }
+
+    Integer MAX_ITERATIONS = 100000;
+
+    static long partTwo(Scanner in) {
+        var grid = parse(in);
+
+        var r1 = new ParabolicReflector(grid);
+        var foundCycle = r1.cycleLength();
+
+        long length = foundCycle.end - foundCycle.start;
+        long cycle = ((long) 1000000000 - foundCycle.start) % length;
+
+        System.out.printf("length: %d, warmup: %d, spin: %d", length, foundCycle.start, cycle);
+
+        var r2 = new ParabolicReflector(grid);
+        for (int i = 0; i < foundCycle.start + cycle; i++) {
+            r2.cycle();
+        }
+
+        return r2.totalLoad();
+    }
+
+    record Cycle(int start, int end) {};
+
+    Cycle cycleLength() {
+        Map<String, Integer> seen = new HashMap<>();
+        for (int i = 0; i < MAX_ITERATIONS; i++) {
+            cycle();
+
+            String got = render();
+            if (seen.containsKey(got)) {
+                return new Cycle(seen.get(got), i);
+            }
+
+            seen.put(got, i);
+        }
+
+        throw new RuntimeException("max iterations, not stable");
+    }
+
+    void cycle() {
+        tiltNorth();
+        tiltWest();
+        tiltSouth();
+        tiltEast();
     }
 
     public void tiltNorth() {
@@ -32,20 +108,94 @@ public class ParabolicReflector {
                 }
             }
         }
+
+        if (!countObjects().equals(counts)) throw new RuntimeException();
     }
 
-    public int totalLoad() {
-        int[] rowWeight = new int[grid.size()];
+    public void tiltWest() {
+        for (int i = 0; i < grid.get(0).size(); i++) {
+            int slideTo = grid.size();
 
-        for (int j = 0 ; j < grid.get(0).size(); j++) {
-            for (int i = 0; i < grid.size(); i++) {
-                if (grid.get(i).get(j) == 'O') {
-                    rowWeight[i] += grid.size() - i;
+            for (int j = 0; j < grid.size(); j++) {
+                if (grid.get(i).get(j) == '#') {
+                    slideTo = j + 1;
+                } else if (grid.get(i).get(j) == '.') {
+                    if (slideTo > j) {
+                        slideTo = j;
+                    }
+                } else if (grid.get(i).get(j) == 'O') {
+                    if (slideTo < j) {
+                        grid.get(i).set(slideTo++, 'O'); // swap boulder to slideTo
+                        grid.get(i).set(j, '.'); // replace boulder with empty space
+                    }
+                    // keep advancing slideTo past any obstacles
+                    while (slideTo < grid.size() && grid.get(i).get(slideTo) != '.') slideTo++;
                 }
             }
         }
+        if (!countObjects().equals(counts)) throw new RuntimeException();
 
-        return Arrays.stream(rowWeight).sum();
+    }
+
+    public void tiltEast() {
+        for (int i = 0; i < grid.get(0).size(); i++) {
+            int slideTo = 0;
+
+            for (int j = grid.size() - 1; j >= 0; j--) {
+                if (grid.get(i).get(j) == '#') {
+                    slideTo = j - 1;
+                } else if (grid.get(i).get(j) == '.') {
+                    if (slideTo < j) {
+                        slideTo = j;
+                    }
+                } else if (grid.get(i).get(j) == 'O') {
+                    if (slideTo > j) {
+                        grid.get(i).set(slideTo--, 'O'); // swap boulder to slideTo
+                        grid.get(i).set(j, '.'); // replace boulder with empty space
+                    }
+                    // keep advancing slideTo past any obstacles
+                    while (slideTo >= 0 && grid.get(i).get(slideTo) != '.') slideTo--;
+                }
+            }
+        }
+        if (!countObjects().equals(counts)) throw new RuntimeException();
+
+    }
+
+    public void tiltSouth() {
+        for (int j = 0; j < grid.get(0).size(); j++) {
+            int slideTo = 0;
+
+            for (int i = grid.size() - 1; i >= 0; i--) {
+                if (grid.get(i).get(j) == '#') {
+                    slideTo = i - 1;
+                } else if (grid.get(i).get(j) == '.') {
+                    if (slideTo < i) {
+                        slideTo = i;
+                    }
+                } else if (grid.get(i).get(j) == 'O') {
+                    if (slideTo > i) {
+                        grid.get(slideTo--).set(j, 'O'); // swap boulder to slideTo
+                        grid.get(i).set(j, '.'); // replace boulder with empty space
+                    }
+                    // keep advancing slideTo past any obstacles
+                    while (slideTo >= 0 && grid.get(slideTo).get(j) != '.') slideTo--;
+                }
+            }
+        }
+        if (!countObjects().equals(counts)) throw new RuntimeException();
+    }
+
+    public long totalLoad() {
+        long weight = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            long boulders = grid.get(i).stream().filter(x -> x == 'O').count();
+
+            weight += boulders * (grid.size() - i);
+        }
+
+        return weight;
     }
 
     String getColumn(int j) {
--- src/test/java/com/jacobsevart/aoc/ParabolicReflectorTest.java
+++ src/test/java/com/jacobsevart/aoc/ParabolicReflectorTest.java
@@ -47,6 +47,154 @@ class ParabolicReflectorTest {
     }
 
     @Test
+    public void testCycle() {
+        var reflector = new ParabolicReflector(new Scanner(testInput));
+        reflector.cycle();
+
+        String expectedOneCycle = """
+                .....#....
+                ....#...O#
+                ...OO##...
+                .OO#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#....
+                ......OOOO
+                #...O###..
+                #..OO#....""";
+
+        assertEquals(expectedOneCycle, reflector.render());
+
+        String expectedTwoCycle = """
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #..OO###..
+                #.OOO#...O""";
+
+        reflector.cycle();
+        assertEquals(expectedTwoCycle, reflector.render());
+
+        String expectedThreeCycle = """
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #...O###.O
+                #.OOO#...O""";
+
+        reflector.cycle();
+        assertEquals(expectedThreeCycle, reflector.render());
+    }
+
+    @Test
+    public void testPartTwoSmall() {
+        assertEquals(64, ParabolicReflector.partTwo(new Scanner(testInput)));
+    }
+
+    @Test
+    public void testCycleSmall() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day14.txt");
+        assertNotNull(txtFile);
+
+        var reflector = new ParabolicReflector(new Scanner(txtFile));
+
+        for (int i = 0; i < 250; i++) {
+            reflector.cycle();
+            System.out.printf("%2d: %5d\n", i, reflector.totalLoad());
+        }
+    }
+
+    @Test
+    public void testCycleSymmetric() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day14.txt");
+        assertNotNull(txtFile);
+        var grid = ParabolicReflector.parse(new Scanner(txtFile));
+
+        ParabolicReflector reflector = new ParabolicReflector(grid);
+        reflector.tiltWest();
+        String referenceWest = reflector.render();
+        reflector.tiltEast();
+        reflector.tiltWest();
+        assertEquals(referenceWest, reflector.render());
+
+        reflector = new ParabolicReflector(grid);
+        reflector.tiltEast();
+        String referenceEast = reflector.render();
+        reflector.tiltWest();
+        reflector.tiltEast();;
+        assertEquals(referenceEast, reflector.render());
+
+        reflector = new ParabolicReflector(grid);
+        reflector.tiltSouth();
+        String referenceSouth = reflector.render();
+        reflector.tiltNorth();
+        reflector.tiltSouth();;
+        assertEquals(referenceSouth, reflector.render());
+
+        reflector = new ParabolicReflector(grid);
+        reflector.tiltNorth();
+        String referenceNorth = reflector.render();
+        System.out.println(referenceNorth);
+        reflector.tiltSouth();
+        reflector.tiltNorth();
+        assertEquals(referenceNorth, reflector.render());
+    }
+
+    @Test
+    public void testCycleIdempotent() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day14.txt");
+        assertNotNull(txtFile);
+        var grid = ParabolicReflector.parse(new Scanner(txtFile));
+
+        ParabolicReflector reflector = new ParabolicReflector(grid);
+        reflector.tiltWest();
+        String referenceWest = reflector.render();
+        reflector.tiltWest();
+        assertEquals(referenceWest, reflector.render());
+
+        reflector = new ParabolicReflector(grid);
+        reflector.tiltEast();
+        String referenceEast = reflector.render();
+        reflector.tiltEast();;
+        assertEquals(referenceEast, reflector.render());
+
+        reflector = new ParabolicReflector(grid);
+        reflector.tiltSouth();
+        String referenceSouth = reflector.render();
+        reflector.tiltSouth();;
+        assertEquals(referenceSouth, reflector.render());
+
+        reflector = new ParabolicReflector(grid);
+        reflector.tiltNorth();
+        String referenceNorth = reflector.render();
+        reflector.tiltNorth();
+        assertEquals(referenceNorth, reflector.render());
+    }
+
+    @Test
+    public void testPartTwoLarge() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day14.txt");
+        assertNotNull(txtFile);
+
+        // I'm not dropping any objects
+        // The tilt operations are symmetric
+        // The tilt operations are idempotent
+        // Fairly high confidence in the load function since already worked on this size input
+
+        assertEquals(0, ParabolicReflector.partTwo(new Scanner(txtFile)));
+    }
+
+    @Test
     public void testPartOneLarge() {
         InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day14.txt");
         assertNotNull(txtFile);
