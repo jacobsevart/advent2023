d5acd6518e86425ad1534769e4d8a3ac7fe654b4
Jacob Sevart <jacob.sevart@gmail.com>
2023-12-16 20:51:31 -0500

    Solve day 15 part 2

--- src/main/java/com/jacobsevart/aoc/Hashing.java
+++ src/main/java/com/jacobsevart/aoc/Hashing.java
@@ -1,18 +1,95 @@
 package com.jacobsevart.aoc;
 
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Hashing {
-    int sum;
+    sealed interface Command permits MinusCommand, EqualsCommand {};
+    record MinusCommand(String label) implements Command {};
+    record EqualsCommand(String label, int focalLength) implements Command {};
 
-    public Hashing(Scanner in) {
-        sum = 0;
+    record Lens(String label, int focalLength) {
+        @Override
+        public String toString() {
+            return String.format("[%s %d]", label, focalLength);
+        }
+    };
+
+    List<List<Lens>> boxes;
+
+    public Hashing() {
+        boxes = new ArrayList<>(256);
+        for (int i = 0; i < 256; i++) {
+            boxes.add(new LinkedList<>());
+        }
+    }
+
+    void execute(Command c) {
+        switch(c) {
+            case MinusCommand m:
+                boxes.get(hash(m.label)).removeIf(x -> x.label.equals(m.label));
+                break;
+            case EqualsCommand e:
+                var h = hash(e.label);
+                for (int i = 0; i < boxes.get(h).size(); i++) {
+                    if (boxes.get(h).get(i).label.equals(e.label)) {
+                        boxes.get(h).set(i, new Lens(e.label, e.focalLength));
+                        return;
+                    }
+                }
+
+                // assume not found
+                boxes.get(h).add(new Lens(e.label, e.focalLength));
+        }
+    }
+
+    int focusingPower() {
+        int acc = 0;
+        for (int i = 0; i < boxes.size(); i++) {
+            for (int j = 0; j < boxes.get(i).size(); j++) {
+                acc += (i + 1) * (j + 1) * boxes.get(i).get(j).focalLength;
+            }
+        }
+
+        return acc;
+    }
+
+    void render() {
+        for (int i = 0; i < boxes.size(); i++) {
+            if (!boxes.get(i).isEmpty()) {
+                System.out.printf("box %d: %s\n", i, boxes.get(i));
+            }
+        }
+    }
+
+    public static List<Command> parse(Scanner in) {
+        List<Command> cmds = new ArrayList<>();
+        in.useDelimiter(",");
+        while (in.hasNext()) {
+            String cmd = in.next();
+
+            if (cmd.contains("-")) {
+                cmds.add(new MinusCommand(cmd.replace("-", "")));
+            } else {
+                String[] parts = cmd.split("=");
+                cmds.add(new EqualsCommand(parts[0], Integer.parseInt(parts[1])));
+            }
+        }
+
+        return cmds;
+    }
+
+
+    public static int hashMany(Scanner in) {
+        int sum = 0;
         in.useDelimiter(",");
         while (in.hasNext()) {
             String s = in.next();
             int h = hash(s);
             sum += h;
         }
+
+        return sum;
     }
 
     public static int hash(String in) {
--- src/test/java/com/jacobsevart/aoc/HashingTest.java
+++ src/test/java/com/jacobsevart/aoc/HashingTest.java
@@ -3,6 +3,7 @@ package com.jacobsevart.aoc;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +19,7 @@ class HashingTest {
 
     @Test
     public void testPartOne() {
-        assertEquals(1320, new Hashing(new Scanner(testInput)).sum);
+        assertEquals(1320, Hashing.hashMany(new Scanner(testInput)));
     }
 
     @Test
@@ -26,8 +27,37 @@ class HashingTest {
         InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day15.txt");
         assertNotNull(txtFile);
 
-        assertEquals(510388, new Hashing(new Scanner(txtFile)).sum);
+        assertEquals(510388, Hashing.hashMany(new Scanner(txtFile)));
     }
 
+    @Test
+    public void testParse() {
+        List<Hashing.Command> commands = Hashing.parse(new Scanner(testInput));
+        commands.forEach(System.out::println);
+    }
 
+    @Test
+    public void testPartTwoSmall() {
+        Hashing h = new Hashing();
+        List<Hashing.Command> cmds = Hashing.parse(new Scanner(testInput));
+        for (Hashing.Command cmd : cmds) {
+            System.out.println(cmd);
+            h.execute(cmd);
+            h.render();
+        }
+        assertEquals(145, h.focusingPower());
+    }
+
+    @Test
+    public void testPartTwoLarge() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day15.txt");
+        assertNotNull(txtFile);
+
+        Hashing h = new Hashing();
+        List<Hashing.Command> cmds = Hashing.parse(new Scanner(txtFile));
+        for (Hashing.Command cmd : cmds) {
+            h.execute(cmd);
+        }
+        assertEquals(291774, h.focusingPower());
+    }
 }
