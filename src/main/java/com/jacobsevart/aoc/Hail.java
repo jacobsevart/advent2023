e684542bbe76a6c8eac8d7b5a7ac084b20f0536d
Jacob Sevart <jacob.sevart@gmail.com>
2023-12-24 12:14:53 -0500

    Solve day 24 part 1

---
+++ src/main/java/com/jacobsevart/aoc/Hail.java
@@ -0,0 +1,78 @@
+package com.jacobsevart.aoc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Hail {
+    public record Stone(long x, long y, long z, long dx, long dy, long dz) {
+    }
+
+    ;
+
+    public record Coordinate(double x, double y) {
+    }
+
+    ;
+
+    public record Collision(int i, int j, Coordinate position) {
+    }
+
+    ;
+
+    List<Stone> hailstones = new ArrayList<>();
+
+    public Hail(Scanner in) {
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            System.out.println(line);
+            String[] parts = line.split(" @ ");
+
+            String[] posParts = parts[0].split(", ");
+            String[] velParts = parts[1].split(", ");
+
+            hailstones.add(new Stone(
+                    Long.parseLong(posParts[0].trim()),
+                    Long.parseLong(posParts[1].trim()),
+                    Long.parseLong(posParts[2].trim()),
+                    Long.parseLong(velParts[0].trim()),
+                    Long.parseLong(velParts[1].trim()),
+                    Long.parseLong(velParts[2].trim())));
+        }
+    }
+
+    public List<Collision> partOne(long testMin, long testMax) {
+        List<Collision> found = new ArrayList<>();
+
+        for (int i = 0; i < hailstones.size(); i++) {
+            for (int j = i + 1; j < hailstones.size(); j++) {
+                var got = crossPaths(hailstones.get(i), hailstones.get(j));
+                if (got.isPresent()) {
+                    var collision = got.get();
+                    if (collision.x >= testMin && collision.x <= testMax && collision.y >= testMin && collision.y < testMax) {
+                        found.add(new Collision(i, j, collision));
+                    }
+                }
+            }
+        }
+
+        return found;
+    }
+
+    public Optional<Coordinate> crossPaths(Stone a, Stone b) {
+        if (b.dx * a.dy == (a.dx * b.dy)) return Optional.empty();
+
+        double divisor = (b.dx * a.dy) - (a.dx * b.dy);
+
+        double t1 = b.dy * (a.x - b.x) + b.dx * (b.y - a.y);
+        t1 = t1 / divisor;
+
+        double t2 = a.dy * (a.x - b.x) + a.dx * (b.y - a.y);
+        t2 = t2 / divisor;
+
+        if (t1 < 0 || t2 < 0) return Optional.empty();
+
+        return Optional.of(new Coordinate(a.x + a.dx * t1, a.y + a.dy * t1));
+    }
+}
---
+++ src/test/java/com/jacobsevart/aoc/HailTest.java
@@ -0,0 +1,53 @@
+package com.jacobsevart.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HailTest {
+    String testInput = """
+            19, 13, 30 @ -2,  1, -2
+            18, 19, 22 @ -1, -1, -2
+            20, 25, 34 @ -2, -2, -4
+            12, 31, 28 @ -1, -2, -1
+            20, 19, 15 @  1, -5, -3""";
+
+    @Test
+    public void testParse() {
+        var hail = new Hail(new Scanner(testInput));
+        hail.hailstones.forEach(System.out::println);
+    }
+
+    @Test
+    public void testIntersect() {
+        var hail = new Hail(new Scanner(testInput));
+
+        Optional<Hail.Coordinate> t = hail.crossPaths(hail.hailstones.get(0), hail.hailstones.get(1));
+        assertTrue(t.isPresent());
+        assertEquals(14.333, t.get().x(), 0.001);
+        assertEquals(15.333, t.get().y(), 0.001);
+    }
+
+    @Test
+    public void testPartOneSmall() {
+        var hail = new Hail(new Scanner(testInput));
+
+        var list = hail.partOne(7, 27);
+        list.forEach(System.out::println);
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testPartOneLarge() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day24.txt");
+        assertNotNull(txtFile);
+
+        var hail = new Hail(new Scanner(txtFile));
+        var list = hail.partOne(200000000000000L, 400000000000000L);
+
+        assertEquals(1, list.size());
+    }
+}
