e222c78f23652fdda5b7ddd54b2ec44b4e25414d
Jacob Sevart <jacob.sevart@gmail.com>
2023-12-24 00:18:04 -0500

    Refactor day 23 code

--- src/main/java/com/jacobsevart/aoc/LongWalk.java
+++ src/main/java/com/jacobsevart/aoc/LongWalk.java
@@ -2,6 +2,7 @@ package com.jacobsevart.aoc;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LongWalk {
@@ -40,31 +41,52 @@ public class LongWalk {
         endNode =  new LongWalk.Coordinate(grid.size() - 1, grid.get(0).size() - 2);
     }
 
-    int partTwo() {
-        var reduction = discoverNodes();
-        discoverAdditionalEdges(reduction);
+    int longestPath(Function<Coordinate, Stream<Coordinate>> neighborProvider) {
+        // first, DFS the graph to discover the nodes where we have an actual choice
+        var reduction = discoverNodes(neighborProvider);
+
+        // second, explore from all sources to discover additional links into these nodes that were suppressed by the "visited" set originally
+        discoverAdditionalEdges(reduction, neighborProvider);
+
         System.out.printf("number of nodes: %d\n", reduction.size());
 
+        // dot visualization
         for (var entry : reduction.entrySet()) {
             for (var node : entry.getValue()) {
                 System.out.printf("\"%s\" -> \"%s\" [label=%d];\n", entry.getKey(), node, node.length);
             }
         }
 
+        // count edges
         int edges = reduction.values().stream().map(Set::size).reduce(0, Integer::sum);
         System.out.printf("number of edges: %d\n", edges);
 
+        // there's no longest-simple-path algorithm, just DFS to produce all paths & choose the max length
         var paths = allPaths(reduction);
         System.out.printf("number of paths: %d\n", paths.size());
 
-        return paths.stream().map(x -> x.stream().map(CoordinateWithLength::length).reduce(0, Integer::sum)).max(Comparator.naturalOrder()).get();
+        var longestPath = paths
+                .stream()
+                .max(Comparator.comparing(LongWalk::pathLength))
+                .get();
+
+        for (CoordinateWithLength node : longestPath) {
+            grid.get(node.x).set(node.y, '*');
+        }
+
+        System.out.println(render());
+
+        return pathLength(longestPath);
     }
+
+    static int pathLength(List<CoordinateWithLength> path) {
+        return path.stream().map(x -> x.length).reduce(0, Integer::sum);
+    }
+
 
     List<List<CoordinateWithLength>> allPaths(Map<Coordinate, Set<CoordinateWithLength>> edges) {
         List<List<CoordinateWithLength>> allPaths = new ArrayList<>();
-
         dfsAllPaths(new CoordinateWithLength(startNode.x, startNode.y, 0), new HashSet<>(), new ArrayList<>(), allPaths, edges);
-
         return allPaths;
     }
 
@@ -90,55 +112,8 @@ public class LongWalk {
         }
     }
 
-    public Map<Coordinate, Set<CoordinateWithLength>> reduceGraph(Function<Coordinate, Stream<Coordinate>> neighborFinder) {
-        Map<Coordinate, Set<CoordinateWithLength>> edges = new HashMap<>();
-        Set<Coordinate> visited = new HashSet<>();
 
-        reduceGraph(startNode, startNode, 0, visited, edges, neighborFinder);
-//        checkDag(start, edges, new ArrayList<>());
-
-        return edges;
-    }
-
-    public void reduceGraph(Coordinate start, Coordinate here, int length, Set<Coordinate> visited, Map<Coordinate, Set<CoordinateWithLength>> edges, Function<Coordinate, Stream<Coordinate>> neighborFinder) {
-        if (visited.contains(here)) return;
-
-        visited.add(here);
-
-        if (here.equals(endNode)) {
-            if (!edges.containsKey(start)) edges.put(start, new HashSet<>());
-
-            edges.get(start).add(new CoordinateWithLength(here.x, here.y, length));
-            edges.put(here, new HashSet<>()); // no outward edges, but so it will show up
-        }
-
-        List<Coordinate> uniqueNeighbors = boundsChecked(neighborFinder.apply(here)).stream().filter(x -> !visited.contains(x)).filter(x -> !x.equals(start)).toList();
-        if (uniqueNeighbors.isEmpty()) {
-            visited.remove(here);
-            return;
-        }
-
-        if (uniqueNeighbors.size() == 1) {
-            reduceGraph(start, uniqueNeighbors.get(0), length + 1, visited, edges, neighborFinder);
-            visited.remove(here);
-            return;
-        }
-
-        CoordinateWithLength newLink = new CoordinateWithLength(here.x, here.y, length + 1);
-        if (!edges.getOrDefault(start, Set.of()).contains(newLink)) {
-            System.out.printf("add edge! %s -> %s\n", start, here);
-        }
-
-        edges.putIfAbsent(start, new HashSet<>());
-        edges.get(start).add(newLink);
-        for (Coordinate n : uniqueNeighbors) {
-            reduceGraph(here, n, 0, visited, edges, neighborFinder);
-        }
-
-        visited.remove(here);
-    }
-
-    public Map<Coordinate, Set<CoordinateWithLength>> discoverNodes() {
+    public Map<Coordinate, Set<CoordinateWithLength>> discoverNodes(Function<Coordinate, Stream<Coordinate>> neighborFinder) {
         Map<Coordinate, Set<CoordinateWithLength>> edges = new HashMap<>();
         Set<Coordinate> visited = new HashSet<>();
 
@@ -159,7 +134,7 @@ public class LongWalk {
                 continue;
             }
 
-            List<Coordinate> uniqueNeighbors = boundsChecked(neighborsPartTwo(frame.here)).stream().filter(x -> !visited.contains(x)).filter(x -> !x.equals(frame.here)).toList();
+            List<Coordinate> uniqueNeighbors = boundsChecked(neighborFinder.apply(frame.here)).stream().filter(x -> !visited.contains(x)).filter(x -> !x.equals(frame.here)).toList();
             if (uniqueNeighbors.isEmpty()) continue;
 
             if (uniqueNeighbors.size() == 1) {
@@ -177,7 +152,9 @@ public class LongWalk {
         return edges;
     }
 
-    public void discoverAdditionalEdges(Map<Coordinate, Set<CoordinateWithLength>> edges) {
+    public void discoverAdditionalEdges(Map<Coordinate, Set<CoordinateWithLength>> edges, Function<Coordinate, Stream<Coordinate>> neighborProvider) {
+        // In the first pass we visited each edge once, but edges may have in-degree > 1. A full graph traversal takes too long
+        // but we can run an exploration from each node with a fresh Visited set, stopping when we reach any other turning point.
         Set<LongWalk.Coordinate> nodeSet = new HashSet<>();
         for (var entry : edges.entrySet()) {
             nodeSet.add(entry.getKey());
@@ -187,68 +164,46 @@ public class LongWalk {
             }
         }
 
-        for (var node : nodeSet) {
-            checkConnectivity(node, nodeSet, edges);
-        }
-    }
+        for (var from : nodeSet) {
+            Set<Coordinate> visited = new HashSet<>();
+            Stack<CoordinateWithLength> stk = new Stack<>();
 
-    void checkConnectivity(Coordinate from, Set<Coordinate> stoppingPoints, Map<Coordinate, Set<CoordinateWithLength>> edges) {
-        Set<Coordinate> visited = new HashSet<>();
-        Stack<CoordinateWithLength> stk = new Stack<>();
+            stk.add(new CoordinateWithLength(from.x, from.y, 0));
 
-        stk.add(new CoordinateWithLength(from.x, from.y, 0));
+            while (!stk.isEmpty()) {
+                CoordinateWithLength frame = stk.pop();
 
-        while (!stk.isEmpty()) {
-            CoordinateWithLength frame = stk.pop();
+                Coordinate c = new Coordinate(frame.x, frame.y);
+                if (visited.contains(c)) continue;
+                visited.add(c);
 
-            Coordinate c = new Coordinate(frame.x, frame.y);
-            if (visited.contains(c))     continue;
-            visited.add(c);
+                if (nodeSet.contains(c) && !c.equals(from)) {
+                    edges.putIfAbsent(from, new HashSet<>());
 
-            if (stoppingPoints.contains(c) && !c.equals(from)) {
+                    // look for the same edge with a potentialy different length
+                    Optional<CoordinateWithLength> found = edges.get(from).stream().filter(x -> x.x == frame.x && x.y == frame.y).findAny();
 
-                edges.putIfAbsent(from, new HashSet<>());
-
-                Optional<CoordinateWithLength> found = edges.get(from).stream().filter(x -> x.x == frame.x && x.y == frame.y).findAny();
-                if (found.isPresent()) {
-                    if (frame.length > found.get().length) {
+                    // if our length is longer, we win
+                    if (found.isPresent() && frame.length > found.get().length) {
                         edges.get(from).remove(found.get());
+                        edges.get(from).add(frame);
+                    } else if (found.isEmpty()) {
                         edges.get(from).add(frame);
                     }
 
-                    // avoid adding duplicate
+                    // note: if our length is the same or worse, don't bother
 
-                } else {
-                    edges.get(from).add(frame);
-                    System.out.printf("discover new edge %s -> %s\n", from, c);
+                    // don't explore past known nodes; we'll call again from all nodes as source
+                    continue;
                 }
 
-                continue; // can go further from another traversal
-            }
-
-            for (Coordinate n : boundsChecked(neighborsPartTwo(c))) {
-                stk.add(new CoordinateWithLength(n.x, n.y, frame.length + 1));
+                for (Coordinate n : boundsChecked(neighborProvider.apply(c))) {
+                    stk.add(new CoordinateWithLength(n.x, n.y, frame.length + 1));
+                }
             }
         }
     }
 
-
-    public void checkDag(Coordinate ptr, Map<Coordinate, Set<CoordinateWithLength>> edges, List<Coordinate> path) {
-        if (path.contains(ptr)) throw new RuntimeException(String.format("not a dag: %s :  %s", ptr, path));
-
-        path.add(ptr);
-
-        if (!edges.containsKey(ptr)) {
-            System.out.printf("no edges out of %s\n", ptr);
-            return;
-        }
-
-        for (var edge : edges.get(ptr)) {
-            checkDag(new Coordinate(edge.x, edge.y), edges, path);
-        }
-
-        path.remove(ptr);
-    }
 
     List<Coordinate> boundsChecked(Stream<Coordinate> s) {
         return s
@@ -286,46 +241,6 @@ public class LongWalk {
                 new Coordinate(c.x + 1, c.y),
                 new Coordinate(c.x, c.y -1),
                 new Coordinate(c.x, c.y + 1));
-    }
-
-    public int longestPath(Coordinate origin, Coordinate destination, Map<Coordinate, Set<CoordinateWithLength>> edges) {
-        Map<Coordinate, Integer> paths = new HashMap<>();
-        Map<Coordinate, Coordinate> prev = new HashMap<>();
-
-        int longest = longestPath(origin, destination, paths, prev, edges);
-
-        Coordinate ptr = destination;
-        while (ptr != origin) {
-            ptr = prev.get(ptr);
-            if (ptr == null) {
-                break;
-            }
-            grid.get(ptr.x).set(ptr.y, 'O');
-        }
-
-        return longest;
-    }
-
-    private static int longestPath(Coordinate origin, Coordinate destination, Map<Coordinate, Integer> paths, Map<Coordinate, Coordinate> prev, Map<Coordinate, Set<CoordinateWithLength>> edges) {
-        // https://jeffe.cs.illinois.edu/teaching/algorithms/book/06-dfs.pdf
-
-        if (origin.equals(destination)) return 0;
-
-        if (!paths.containsKey(origin)) {
-            paths.put(origin, Integer.MIN_VALUE);
-
-            for (CoordinateWithLength w : edges.getOrDefault(origin, Set.of())) {
-                int replace = w.length + longestPath(new Coordinate(w.x, w.y), destination, paths, prev, edges);
-
-                if (replace > paths.get(origin)) {
-                    paths.put(origin, replace);
-                    prev.put(new Coordinate(w.x, w.y), origin);
-                }
-            }
-
-        }
-
-        return paths.get(origin);
     }
 
     String render(Map<Coordinate, Set<CoordinateWithLength>> edges) {
--- src/test/java/com/jacobsevart/aoc/LongWalkTest.java
+++ src/test/java/com/jacobsevart/aoc/LongWalkTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -37,36 +36,24 @@ class LongWalkTest {
             #####################.#""";
 
     @Test
-    public void testLongestPath() {
+    public void testPartOneSmall() {
         var walk = new LongWalk(new Scanner(testInput));
-
-        LongWalk.Coordinate origin = new LongWalk.Coordinate(0, 1);
-        LongWalk.Coordinate destination =  new LongWalk.Coordinate(walk.grid.size() - 1, walk.grid.get(0).size() - 2);
-
-        var dag = walk.reduceGraph(walk::neighborsPartOne);
-
-        var longestPath = walk.longestPath(origin, destination, dag);
-        System.out.println(walk.render());
-
-        assertEquals(94, longestPath);
+        assertEquals(94, walk.longestPath(walk::neighborsPartOne));
     }
 
     @Test
     public void testPartTwoSmall() {
         var walk = new LongWalk(new Scanner(testInput));
-        assertEquals(154, walk.partTwo());
+        assertEquals(154, walk.longestPath(walk::neighborsPartTwo));
     }
 
     @Test
-    public void testPartTwoSmallOldWay() {
-        var walk = new LongWalk(new Scanner(testInput));
-        var reduction = walk.reduceGraph(walk::neighborsPartTwo);
-        System.out.printf("number of nodes: %d\n", reduction.size());
-        System.out.printf("number of edges: %d\n", reduction.values().stream().map(Set::size).reduce(0, Integer::sum));
-        var paths = walk.allPaths(reduction);
-        System.out.printf("number of paths: %d\n", paths.size());
+    public void testPartOneLarge() {
+        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day23.txt");
+        assertNotNull(txtFile);
 
-        assertEquals(154, paths.stream().map(x -> x.stream().map(y -> y.length()).reduce(0, Integer::sum)).max(Comparator.naturalOrder()).get());
+        var walk = new LongWalk(new Scanner(txtFile));
+        assertEquals(2318, walk.longestPath(walk::neighborsPartOne));
     }
 
     @Test
@@ -75,47 +62,7 @@ class LongWalkTest {
         assertNotNull(txtFile);
 
         var walk = new LongWalk(new Scanner(txtFile));
-        assertEquals(6426, walk.partTwo()); // 14 seconds
+        assertEquals(6426, walk.longestPath(walk::neighborsPartTwo)); // 14 seconds
     }
 
-    @Test
-    public void testDagReductionSmall() {
-        var walk = new LongWalk(new Scanner(testInput));
-        LongWalk.Coordinate origin = new LongWalk.Coordinate(0, 1);
-        LongWalk.Coordinate destination =  new LongWalk.Coordinate(walk.grid.size() - 1, walk.grid.get(0).size() - 2);
-
-        var dag = walk.reduceGraph(walk::neighborsPartOne);
-        System.out.println(walk.render(dag));
-        dag.entrySet().forEach(System.out::println);
-    }
-
-    @Test
-    public void testDagReductionLarge() {
-        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day23.txt");
-        assertNotNull(txtFile);
-
-        var walk = new LongWalk(new Scanner(txtFile));
-        LongWalk.Coordinate origin = new LongWalk.Coordinate(0, 1);
-        LongWalk.Coordinate destination =  new LongWalk.Coordinate(walk.grid.size() - 1, walk.grid.get(0).size() - 2);
-        var dag = walk.reduceGraph(walk::neighborsPartOne);
-        dag.entrySet().forEach(System.out::println);
-        System.out.println(walk.render(dag));
-        System.out.println(dag.size());
-    }
-
-    @Test
-    public void testLongestPathLarge() {
-        InputStream txtFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("day23.txt");
-        assertNotNull(txtFile);
-
-        var walk = new LongWalk(new Scanner(txtFile));
-        LongWalk.Coordinate origin = new LongWalk.Coordinate(0, 1);
-        LongWalk.Coordinate destination =  new LongWalk.Coordinate(walk.grid.size() - 1, walk.grid.get(0).size() - 2);
-
-
-        var dag = walk.reduceGraph(walk::neighborsPartOne);
-        var path = walk.longestPath(origin, destination, dag);
-        assertEquals(2318, path);
-        System.out.println(walk.render());
-    }
 }
